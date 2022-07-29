dacfd741ec21a99f6f90357da40c9dacafcdcdd4
Jakob Luter <jakobluter20@gmail.com>
2022-07-29 14:10:44 -0500

    Add routing

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,8 @@
 import './Navigation.scss';
+import {Link} from 'react-router-dom';
 
 const Navigation = () => {
+    
     return (
  
         <ul className="navigation__list">
@@ -8,7 +10,7 @@ const Navigation = () => {
             <li className="navigation__list-item">Medical History</li>
             <li className="navigation__list-item"> Calendar</li>
             <li className="navigation__list-item"> My Doctor</li>
-            <li className="navigation__list-item"> Appointment</li>
+            <li className="navigation__list-item"> <Link to="/health/appointment">Appointment</Link></li>
             <li className="navigation__list-item">Settings</li>
             <li className="navigation__list-item">Help</li>
         </ul>
