93ff9cd980d22c86647e9424d47906e9ce76045a
Marco Ye <marco.ye99@gmail.com>
2022-07-29 04:23:28 -0400

    rough draft of appointment page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Appointment from './components/Appointment/Appointment';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Appointment/>
     </div>
   );
 }
