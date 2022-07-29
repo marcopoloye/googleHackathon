7cfff3cbf9da375416be887c3bc2eef75d3539ab
Jakob Luter <jakobluter20@gmail.com>
2022-07-29 10:10:18 -0500

    Adjust calendarArray to hold more information. Adjust day-bar and add month title plus assets folder.

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -2,50 +2,111 @@ import React from 'react';
 import uuid from 'react-uuid';
 import './Calendar.scss';
 import DateCard from '../DateCard/DateCard';
+import NextButton from '../../assets/Next.png';
 
 const Calendar = () => {
 
 
       // calendarArray has been made in this format to add other months as objects if needed, or other key/value pairs besides the days if needed.
     const calendarArray = 
-    {July: 
+    {August: 
       {Days: [
-        "26",
-        "27",
-        "28",
-        "29",
-        "30",
-        "01",
-        "02",
-        "03",
-        "04",
-        "05",
-        "06",
-        "07",
-        "08",
-        "09",
-        "10",
-        "11",
-        "12",
-        "13",
-        "14",
-        "15",
-        "16",
-        "17",
-        "18",
-        "19",
-        "20",
-        "21",
-        "22",
-        "23",
-        "24",
-        "25",
-        "26",
-        "27",
-        "28",
-        "29",
-        "30",
-        "31",
+        {isDoctorAvailable: false,
+        dayNumber: "",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "1",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "2",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "3",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "4",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "5",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "6",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "7",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "8",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "9",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "10",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "11",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "12",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "13",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "14",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "15",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "16",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "17",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "18",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "19",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "20",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "21",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "22",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "23",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "24",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "25",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "26",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "27",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "28",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "29",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "30",
+        isDoctorScheduled: false},
+        {isDoctorAvailable: false,
+        dayNumber: "31",
+        isDoctorScheduled: false}
       ]}
     }
 
@@ -55,24 +116,28 @@ const Calendar = () => {
     return (
         <section className="calendar-page">
             <img className="calendar-page__back-button" alt="Back Arrow Button"/>
+            <article className='calendar-page__dr-title'>Appointment with Dr. John Doe</article>
             <form className="calendar-form"> 
-                <article className='calendar-form__dr-title'>Appointment with Dr. John Doe</article>
+                <div className='calendar-form__month-title-wrapper'>
+                    <h3 className='calendar-form__month-title'>August 2022</h3>
+                    <img className='calendar-form__next-button' alt="Next Month Button" src={NextButton}/>
+                </div>
                 <article className='calendar-form__day-bar'>
-                    <div className='calendar-form__day-bar--day'>Sunday</div>
-                    <div className='calendar-form__day-bar--day'>Monday</div>
-                    <div className='calendar-form__day-bar--day'>Tuesday</div>
-                    <div className='calendar-form__day-bar--day'>Wednesday</div>
-                    <div className='calendar-form__day-bar--day'>Thursday</div>
-                    <div className='calendar-form__day-bar--day'>Friday</div>
-                    <div className='calendar-form__day-bar--day'>Saturday</div>
+                    <div className='calendar-form__day-bar--day'>S</div>
+                    <div className='calendar-form__day-bar--day'>M</div>
+                    <div className='calendar-form__day-bar--day'>T</div>
+                    <div className='calendar-form__day-bar--day'>W</div>
+                    <div className='calendar-form__day-bar--day'>T</div>
+                    <div className='calendar-form__day-bar--day'>F</div>
+                    <div className='calendar-form__day-bar--day'>S</div>
                 </article>
                 <article className='calendar-form__calendar-wrapper'>
-                    {calendarArray.July.Days.map(day => {
+                    {calendarArray.August.Days.map(day => {
                         return (
                             <DateCard 
                                 calendarArray={calendarArray} 
                                 key={uuid()}
-                                day={day}
+                                day={day.dayNumber}
                             />
                         )
                     })}
