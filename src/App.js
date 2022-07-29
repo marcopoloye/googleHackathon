489b02813e235f272cfa85348433c24b78de77d8
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-07-29 11:20:53 -0400

    Built nav component with styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Calendar from './components/Calendar/Calendar';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Navigation from './components/Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
       <Router className="App">
       <Header />
+      <Navigation/>
         <Switch>
           <Route path="/calendar" component={Calendar}/>
         </Switch>
