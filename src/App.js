510f0e5225864d2c4e57ae632a99bf7faf72a173
Marco Ye <marco.ye99@gmail.com>
2022-07-29 05:56:03 -0400

    finished appointment page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Calendar from './components/Calendar/Calendar';
 import Header from './components/Header/Header';
 import Appointment from './components/Appointment/Appointment';
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Appointment/>
-    </div>
+      <Switch>
+        <Route path="/calendar" component={Calendar}/>
+        <Route path="/appointment" component={Appointment}/>
+      </Switch>
+    </Router>
   );
 }
+
 
 export default App;
