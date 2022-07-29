71b33ae2c411da19d560efb6a9c4dddcd5196323
Victory Onuoha <Victoryonuoha@cmail.carleton.ca>
2022-07-29 09:50:23 -0400

    Created Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+
+const Footer = () => {
+    return (
+        <main>
+            <ul>
+                <li>Privacy</li>
+                <li>Terms</li>
+                <li>Help</li>
+                <li>About</li>
+            </ul>
+        </main>
+    )
+}
+
+export default Footer;
