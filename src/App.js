7ac682fbabc73081ef3c58856c24e2648518c74b
Marco Ye <marco.ye99@gmail.com>
2022-07-29 01:20:04 -0400

    added a couple of routes to start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Header from './components/Header/Header';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Appointment from './components/Appointment/Appointment';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-    </div>
+      <Switch>
+        <Route path= '/' />
+        <Route path= '/appointment' component= {Appointment}/>
+      </Switch>
+    </Router>
+      
+
   );
 }
 
