35af74b4ac12bd692934deb762f769983bfbdcca
Marco Ye <marco.ye99@gmail.com>
2022-09-25 05:13:38 -0400

    added alt text to images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     return (
         <header className='header' >
             <section className='header__row' >
-                <img src={Logo} className='header__logo' avatar='Google logo' />
+                <img src={Logo} className='header__logo' alt='Google logo' />
                 <h1 className='header__title' >Health Connect
                 <span className='header__patient'>Patient</span>
                 </h1>
@@ -21,7 +21,7 @@ const Header = () => {
 
             <section className='header__row-avatar' >
                 <article> 
-                    <img src={bento} className='header__bento' />
+                    <img src={bento} className='header__bento' alt='bento icons'/>
                 </article>
                 <article className='header__avatar-box'> 
                     <img src={userAvatar} 
